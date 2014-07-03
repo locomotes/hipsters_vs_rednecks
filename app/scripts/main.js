1f9ddb263353f8315f1f2ebe262ef51832b472d5
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-03 15:54:06 -0400

    survey completed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('The Iron Yard Rocks');
+
+
+$('form').on('submit', function(e) {
+	e.preventDefault();
+	var redneck = 0;
+	var hipster = 0;
+	$(':radio:checked').each(function() {
+
+			if ($(this).val() === 'r') {
+				redneck += 1;
+			} else {
+				hipster += 1;
+			}
+	});
+
+			if (redneck > hipster) {
+				alert("YOU'RE A REDNECK");
+				$(".#surveybox").hide();
+			} else {
+				alert("YOU'RE A HIPSTER");
+			}
+
+});
+
+var redgame = $(".#surveybox").hide();
