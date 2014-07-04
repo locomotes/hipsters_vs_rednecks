fb0f6f12ef4232ba7fa5ce6159da09df9c026152
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-04 12:37:30 -0400

    redenck attack button completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,7 @@
 
-// $('form').on('submit', function () {
-  
-//   var redneck = 0;
-//   var hipster = 0;
-  
-//   $(':radio:checked').each(function(){
-        
-//     if ($(this).val() === 'r'){
-//       redneck += 1;
-//     } else {
-//       hipster += 1;
-//     }
-    
-//   });
-  
-//   if (redneck > hipster) {
-//     alert("You're a Redneck");
-//   } else {
-//     alert("You're a Hipster");
-//   }
- 
-  
-// });
 
-$('form').on('submit', function() {
+$('.formone').on('submit', function(e) {
+	e.preventDefault();
 	var redneck = 0;
 	var hipster = 0;
 	$(':radio:checked').each(function() {
@@ -33,12 +11,24 @@ $('form').on('submit', function() {
 			} else {
 				hipster += 1;
 			}
-
+	});
 
 			if (redneck > hipster) {
 				alert("YOU'RE A REDNECK");
+				$('.hero-unit').fadeOut('slow', function() {
+					$('.redneck-unit').fadeIn('slow');
+				});
 			} else {
 				alert("YOU'RE A HIPSTER");
+				$('.hero-unit').fadeOut('slow', function() {
+				$('.hipster-unit').fadeIn('slow');
+				});
 			}
-	});
+
+	
+
 });
+
+$('.attack').on('click', function() {
+		$('#lawnmower').toggleClass('activeone');
+	});
