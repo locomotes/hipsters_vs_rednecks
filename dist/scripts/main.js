f54ca586c3f271bb919dc36f34b2e802e89157d8
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-07 08:56:58 -0400

    ran gulp

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -29,6 +29,58 @@ $('.formone').on('submit', function(e) {
 
 });
 
-$('.attack').on('click', function() {
-		$('#lawnmower').toggleClass('activeone');
-	});
+var hipsterHealth = $('#healthright');
+var attackBTN = $('.attack');
+// var hipsterhealth = health.find('input').val();
+// var damage = _.random(10, 40);
+
+// $('.attack').on('click', function() {
+// 		$('#lawnmower').toggleClass('activeone');
+		
+		
+		
+		
+		
+// });
+// var newhealth = function (a) {
+// 			a.hipsterhealth -= damage;
+// 		};
+// // console.log(damage);
+// // console.log(hipsterhealth);
+// console.log(newhealth);
+
+//Redneck Constructor: 
+var Redneck = function () {
+  this.health = 100;
+  this.attack = function (target) {
+    var damage = _.random(1, 30);
+    console.log(damage);
+    target.health -= damage;
+    if(target.health > 0) {
+      target.monsterElem.find('input').val(target.health);
+      // Monster Needs to Attack Person
+    } else {
+      target.hipsterElem.find('input').val('DEAD');
+      target.hipsterElem.css('color', 'red');
+    }
+  };
+};
+
+//Hipster Constructor:
+var Hipster = function (options) {
+  var options = options || {};
+  this.health = options.health;
+  this.attack = function (target) {
+    // Attack Hipster
+  }
+  this.hipsterElem = options.hipsterElem;
+};
+
+//Hipster Instance:
+var cerra = new Hipster ({
+  health: 30,
+  hipsterElem: minion
+});
+
+
+
